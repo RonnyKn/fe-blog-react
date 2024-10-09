2ef556f2796d1924b5001869fdce35d12bfc1ece
ronnykn <ronnykurniawn@gmail.com>
2024-10-09 23:40:32 +0700

    adding routes blogs details

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,14 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import BlogDetails from "./pages/BlogDetails";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const res = await axios.get("http://localhost:4000");
-      const data = res.data;
-      setData(data?.data);
-    } catch (error) {
-      console.log("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  console.log(data);
-
   return (
     <>
-      <div className="container text-center">
-        <h1 className="text-3xl font-bold">React Gofiber Blog</h1>
-
-        <div>
-          {data.map((item) => (
-            <div key={item?.id} className="flex flex-col py-2 ">
-              <p>{item?.title}</p>
-              <p>{item?.post}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<BlogDetails />} />
+      </Routes>
     </>
   );
 }
